2244388448d9703e443bfc14d37b01eb761e5254
Tony <vuthang@ya.ru>
2014-03-25 23:32:55 -0400

    removed unneeded files

---
+++ src/AutoWeb/Controllers/AboutController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AutoWeb.Controllers
+{
+    public class AboutController : Controller
+    {
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+    }
+}
