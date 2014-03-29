c607ce7ad73f9a90afa88b98f68c3fa067a1c048
Tony <vuthang@ya.ru>
2014-03-29 16:02:36 -0400

    more chnages to base

--- src/AutoWeb/App_Start/Bootstrap.cs
+++ src/AutoWeb/App_Start/Bootstrap.cs
@@ -18,8 +18,8 @@ public class Bootstrap
         App.Cache.Add(string.Format(App._cacheKeyEn, "VEHICLETYPES", "en"), VehicleTypes.Where(x => x.Lang == "en").ToList(), DateTime.Today.AddHours(App._cacheExpiry));
         App.Cache.Add(string.Format(App._cacheKeyEn, "VEHICLETYPES", "fr"), VehicleTypes.Where(x => x.Lang == "fr").ToList(), DateTime.Today.AddHours(App._cacheExpiry));
         App.Cache.Add(string.Format(App._cacheKeyEn, "VEHICLETYPES", "zh"), VehicleTypes.Where(x => x.Lang == "zh").ToList(), DateTime.Today.AddHours(App._cacheExpiry));
-        App.Cache.Add(string.Format(App._cacheKeyEn, "VEHICLETYPES", "de"), VehicleTypes.Where(x => x.Lang == "de").ToList(), DateTime.Today.AddHours(App._cacheExpiry));
-        App.Cache.Add(string.Format(App._cacheKeyEn, "VEHICLETYPES", "it"), VehicleTypes.Where(x => x.Lang == "it").ToList(), DateTime.Today.AddHours(App._cacheExpiry));
+        //App.Cache.Add(string.Format(App._cacheKeyEn, "VEHICLETYPES", "de"), VehicleTypes.Where(x => x.Lang == "de").ToList(), DateTime.Today.AddHours(App._cacheExpiry));
+        //App.Cache.Add(string.Format(App._cacheKeyEn, "VEHICLETYPES", "it"), VehicleTypes.Where(x => x.Lang == "it").ToList(), DateTime.Today.AddHours(App._cacheExpiry));
 
         //List<StatusType> StatusTypes = stdCtx.StatusTypes.Where(x => x.StatusTypeID > 0).OrderBy(x => x.Name).ToList();
         //App.Cache.Add(string.Format(App._cacheKeyEn, "VEHICLETYPES", "en"), StatusTypes, DateTime.Today.AddHours(App._cacheExpiry));
